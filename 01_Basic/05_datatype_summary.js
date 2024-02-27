da300eb16340ec317e604a2d8ecc94b5fc2dda8a
Akashkumar1844 <rajakashcool@gmail.com>
2024-02-27 13:36:29 +0530

    Data_type

---
+++ 01_Basic/05_datatype_summary.js
@@ -0,0 +1,51 @@
+//Mainly two type of Datatype
+
+/*
+ primitive:-
+
+ 7types:- string , Number , Boolean , null, undefined, symbol,BigInt
+
+
+ Non primitive:-
+
+ Array, Objects, functions.
+
+ */
+// integer Datatype
+//  const score = 100;
+//  const scoreValue = 100.3;
+//  const OutsideTemp = null
+//  let userEmail;
+
+//  const id = Symbol('123');
+//  const anotherId = Symbol('123');
+//  console.log(id === anotherId );//false because Symbol having diffent value in different variable type either thier variable data is same or not.
+
+//  const bigNumber = 3456373678n;
+
+
+ //Array
+
+ const heros = ["shaktiman", "naagraj", "doga" ];
+ console.log(heros)
+
+ //object
+
+
+ let myobj ={
+    name: "akash",
+    age: "23",
+ }
+
+ console.log(myobj);
+
+ //Function
+
+ const myFunction = function(){
+    console.log("Hello world");
+ }
+console.log(typeof myFunction);//object function
+
+
+
+
