a7d36758608f6c1f463145729254fbc29a1f11bf
Akashkumar1844 <rajakashcool@gmail.com>
2024-03-01 18:05:52 +0530

    whileloop

---
+++ 04_loops/02_while_dowhile.js
@@ -0,0 +1,31 @@
+// whileloop
+
+let i = 0
+while(i<=10){
+    // console.log(`value of index is ${i}`);
+    i+=2;
+}
+
+
+let MyArray = ['flash','batman','superman']
+let arr = 0
+while(arr<MyArray.length){
+    // console.log(`value is ${MyArray[arr]}`)
+    arr = arr+1;
+}
+
+
+//DoWhileloop kam se kam ek baar loop chalta hi hai
+ let score =1;
+do {
+    // console.log(`score is ${score}`)
+    score++;
+} while (score<10);
+
+
+// kam se kam ek baar loop chalta hi hai
+ let score1 =11;
+do {
+    console.log(`score is ${score1}`)
+    score1++;
+} while (score<10);
